f80194f08b43862d4439015cd8797ba9cc26af1c
ScorpionusVindicta <vindictascorpionus@gmail.com>
2024-08-07 01:37:54 +0200

    Main Files

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
